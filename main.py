5e55059f74e089ca62f15b494e7410fc4970939f
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-05-03 00:12:50 +0100

    Added all current code

---
+++ main.py
@@ -0,0 +1,194 @@
+from kivy.app import App
+
+from kivy.uix.widget import Widget
+from kivy.uix.effectwidget import EffectWidget, FXAAEffect
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.vector import Vector
+from kivy.properties import (ListProperty, DictProperty,
+                             NumericProperty, ObjectProperty,
+                             BooleanProperty)
+
+import kivent
+from kivent import GameSystem
+
+from random import randint
+from math import radians, sqrt, atan2, sin, cos
+from functools import partial
+
+import ipdb
+
+class ProjectileSystem(GameSystem):
+
+    def fire_projectile(self, pos, angle):
+        shape_dict = {'inner_radius': 0,
+                      'outer_radius': 10,
+                      'mass': 50,
+                      'offset': (0, 0)}
+        col_shape = {'shape_type': 'circle',
+                     'elasticity': 1.,
+                     'collision_type': 1,
+                     'shape_info': shape_dict,
+                     'friction': 10.0}
+        col_shapes = [col_shape]
+        angle = self.angle
+        vel = (100*cos(angle), 100*sin(angle))
+        physics_component = {'main_shape': 'circle',
+                             'velocity': vel,
+                             'position': pos,
+                             'angle': self.angle,
+                             'angular_velocity': 0,
+                             'vel_limit': 500,
+                             'ang_vel_limit': radians(200),
+                             'mass': 50,
+                             'col_shapes': col_shapes}
+        create_component_dict = {'physics': physics_component,
+                                 'physics_renderer': {'texture': 'fireball',
+                                                      'size': (20, 20)},
+                                 'position': pos,
+                                 'player': {},
+                                 'rotate': 0}
+        component_order = ['position', 'rotate', 'physics', 'player',
+                           'physics_renderer']
+        result = self.gameworld.init_entity(create_component_dict,
+                                            component_order)
+        return result
+
+class PlayerSystem(GameSystem):
+
+    velocity = ListProperty([0, 0])
+    keys_pressed = ObjectProperty(set())
+    mouse_pos = ListProperty([0, 0])
+    angle = NumericProperty(0)
+
+    updateable = BooleanProperty(True)
+
+    def __init__(self, *args, **kwargs):
+        super(PlayerSystem, self).__init__(*args, **kwargs)
+        Window.bind(on_key_down=self.on_key_down,
+                    on_key_up=self.on_key_up,
+                    mouse_pos=self.setter('mouse_pos'))
+
+    def on_key_down(self, window, keycode, *args):
+        print 'key down', self.velocity
+        if keycode not in (273, 274, 275, 276):
+            return
+        self.keys_pressed.add(keycode)
+        self.recalculate_velocity()
+
+    def on_key_up(self, window, keycode, *args):
+        print 'key up', self.velocity
+        if keycode not in (273, 274, 275, 276):
+            return
+        self.keys_pressed.remove(keycode)
+        self.recalculate_velocity()
+
+    def update(self, dt):
+        super(PlayerSystem, self).update(dt)
+        mp = self.mouse_pos
+        entity = self.gameworld.entities[self.entity_ids[0]]
+        body = entity.physics.body
+        pos = Vector((body.position.x, body.position.y))
+        diff = Vector(mp) - pos
+        angle = atan2(diff[1], diff[0])
+        body.angle = angle
+        self.angle = angle
+
+    def recalculate_velocity(self, *args):
+        vel = Vector([0, 0])
+        for keycode in self.keys_pressed:
+            vel = vel + Vector(
+                {273: (0, 1), 274: (0, -1),
+                 275: (1, 0), 276: (-1, 0)}[keycode])
+        self.velocity = vel
+
+    def on_velocity(self, *args):
+        print 'velocity changed', self.velocity
+        entity = self.gameworld.entities[self.entity_ids[0]]
+        entity.physics.body.velocity = (500*self.velocity[0],
+                                        500*self.velocity[1])
+
+    def on_touch_down(self, touch):
+        entity = self.gameworld.entities[self.entity_ids[0]]
+        print 'pos is', entity.physics.body.position
+        self.gameworld.systems['projectile'].fire_projectile(
+            (entity.position.x, entity.position.y), self.angle)
+    
+    def spawn_player(self, pos):
+        shape_dict = {'inner_radius': 0,
+                      'outer_radius': 32,
+                      'mass': 50,
+                      'offset': (0, 0)}
+        col_shape = {'shape_type': 'circle',
+                     'elasticity': 1.,
+                     'collision_type': 1,
+                     'shape_info': shape_dict,
+                     'friction': 10.0}
+        col_shapes = [col_shape]
+        physics_component = {'main_shape': 'circle',
+                             'velocity': (0, 0),
+                             'position': pos,
+                             'angle': 0,
+                             'angular_velocity': 0,
+                             'vel_limit': 250,
+                             'ang_vel_limit': radians(200),
+                             'mass': 50,
+                             'col_shapes': col_shapes}
+        create_component_dict = {'physics': physics_component,
+                                 'physics_renderer': {'texture': 'black_stone',
+                                                      'size': (64, 64)},
+                                 'position': pos,
+                                 'player': {},
+                                 'rotate': 0}
+        component_order = ['position', 'rotate', 'physics', 'player',
+                           'physics_renderer']
+        result = self.gameworld.init_entity(create_component_dict,
+                                            component_order)
+        return result
+
+class KiventGame(Widget):
+
+    def __init__(self, **kwargs):
+        super(KiventGame, self).__init__(**kwargs)
+        Clock.schedule_once(self.init_game)
+
+    def init_game(self, dt):
+        self.setup_map()
+        self.setup_states()
+        self.set_state()
+        self.gameworld.systems['player'].spawn_player((150, 150))
+        Clock.schedule_interval(self.update, 0)
+
+    def update(self, dt):
+        self.gameworld.update(dt)
+
+    def setup_map(self):
+        gameworld = self.gameworld
+        gameworld.currentmap = gameworld.systems['map']
+
+    def setup_states(self):
+        self.gameworld.add_state(state_name='main',
+                                 systems_added=['debug_renderer',
+                                                'physics_renderer',
+                                                'player'],
+                                 systems_removed=[],
+                                 systems_paused=[],
+                                 systems_unpaused=['debug_renderer',
+                                                   'physics_renderer'],
+                                 screenmanager_screen='main')
+
+    def set_state(self):
+        self.gameworld.state = 'main'
+
+class Interface(BoxLayout):
+    pass
+
+class KiventApp(App):
+    def build(self):
+        Window.clearcolor = (0.1, 0.1, 0.1, 1)
+
+
+
+if __name__ == "__main__":
+    KiventApp().run()
