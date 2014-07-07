d7b1922f337203eea30afdb315dfe503a86d0d18
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-07-07 23:13:45 +0100

    Added some earth shield details

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from kivy.properties import (ListProperty, DictProperty,
 import kivent
 from kivent import GameSystem, DynamicRenderer
 
-from random import randint, choice, random
+from random import randint, choice, random, normalvariate
 from math import radians, sqrt, atan2, sin, cos, pi
 from functools import partial
 
@@ -176,15 +176,16 @@ class ShieldSystem(GameSystem):
         pos = touch.pos
         diff = (pos[0] - points[-2], pos[1] - points[-1])
         dist = sqrt(diff[0]**2 + diff[1]**2)
+        print 'dist is', dist
 
         if dist > self.new_shield_dist:
             points.extend(pos)
 
-        if len(points) > 2:
-            if entity is None:
-                entity = self.init_new_shield(points, texture)
-            else:
-                self.extend_shield(points, entity, texture)
+            if len(points) > 2:
+                if entity is None:
+                    entity = self.init_new_shield(points, texture)
+                else:
+                    self.extend_shield(points, entity, texture)
 
         self.current_touches[touch] = (points, entity, texture)
 
@@ -256,12 +257,12 @@ class EarthShieldSystem(ShieldSystem):
         end = (points[-2], points[-1])
         dr = (end[0] - start[0], end[1] - start[1])
 
-        angle = atan2(dr[1], dr[0])
-        length = sqrt(dr[0]**2 + dr[1]**2)
+        angle = random() * 2*pi
+        length = sqrt(dr[0]**2 + dr[1]**2) / 2.
 
-        random_modifier = 0.4*random()
-        radius = length*(1.2 + random_modifier)
-        mass = 400*(pi*radius**2 / pi*(1.2*length)**2)
+        random_modifier = 0.8*(random()-0.5)
+        radius = length*(1.3 + random_modifier)
+        mass = 1800*(radius/(1.2*length))**2
 
         shape_dict = {'inner_radius': 0,
                       'outer_radius': radius,
@@ -274,36 +275,85 @@ class EarthShieldSystem(ShieldSystem):
                      'friction': 10.0}
         col_shapes = [col_shape]
 
-        centre_x = start[0] + 0.5*length*cos(angle)
-        centre_y = start[1] + 0.5*length*sin(angle)
+        centre_x = (start[0] + 0.5*length*cos(angle) +
+                    0.1*length*normalvariate(0, 1))
+        centre_y = (start[1] + 0.5*length*sin(angle) +
+                    0.1*length*normalvariate(0, 1))
         physics_component = {'main_shape': 'box',
                              'velocity': (0, 0),
                              'position': (centre_x, centre_y),
                              'angle': angle,
                              'angular_velocity': 0,
-                             'vel_limit': 0,
-                             'ang_vel_limit': 0,
-                             'mass': 0,
+                             'vel_limit': 5000,
+                             'ang_vel_limit': 4*pi,
+                             'mass': mass,
                              'col_shapes': col_shapes}
         create_component_dict = {'physics': physics_component,
                                  self.renderer: {'texture': texture,
-                                                 'size': (length, 30)},
+                                                 'size': (2*radius, 2*radius)},
                                  'position': start,
                                  'wall': {},
-                                 'timing': {'original_time': 3.,
-                                            'current_time': 3.},
                                  'color': tuple(self.colour),
                                  'rotate': 0}
         component_order = ['position', 'rotate', 'color', 'physics',
                            'wall',
-                           'timing',
                            self.renderer]
         result = self.gameworld.init_entity(create_component_dict,
                                             component_order)
         return self.gameworld.entities[result]
 
-    def init_secondary_rocks(self, points, texture):
-        pass
+    def touch_move(self, touch):
+        super(EarthShieldSystem, self).touch_move(touch)
+
+        points, entity, texture = self.current_touches[touch]
+        if random() > 0.9 and len(points) >= 4:
+            self.init_secondary_rock(touch.pos, points, texture)
+
+    def init_secondary_rock(self, pos, points, texture):
+        length = 50
+        angle = random() * 2*pi
+        
+        random_modifier = 1.25*(random()-0.5)
+        radius = 0.2*length*(1.3 + random_modifier)
+        mass = 1800*(radius/(1.2*length))**2
+
+        shape_dict = {'inner_radius': 0,
+                      'outer_radius': radius,
+                      'mass': mass,
+                      'offset': (0, 0)}
+        col_shape = {'shape_type': 'circle',
+                     'elasticity': 0.7,
+                     'collision_type': self.collision_type,
+                     'shape_info': shape_dict,
+                     'friction': 10.0}
+        col_shapes = [col_shape]
+
+        centre_x = (pos[0] + 
+                    0.3*length*normalvariate(0, 1))
+        centre_y = (pos[1] + 
+                    0.3*length*normalvariate(0, 1))
+        physics_component = {'main_shape': 'box',
+                             'velocity': (0, 0),
+                             'position': (centre_x, centre_y),
+                             'angle': angle,
+                             'angular_velocity': 0,
+                             'vel_limit': 5000,
+                             'ang_vel_limit': 4*pi,
+                             'mass': mass,
+                             'col_shapes': col_shapes}
+        create_component_dict = {'physics': physics_component,
+                                 self.renderer: {'texture': texture,
+                                                 'size': (2*radius, 2*radius)},
+                                 'position': (centre_x, centre_y),
+                                 'wall': {},
+                                 'color': tuple(self.colour),
+                                 'rotate': 0}
+        component_order = ['position', 'rotate', 'color', 'physics',
+                           'wall',
+                           self.renderer]
+        result = self.gameworld.init_entity(create_component_dict,
+                                            component_order)
+        return self.gameworld.entities[result]
 
     
 class AirShieldSystem(ShieldSystem):
